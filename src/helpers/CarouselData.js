24d31f611b988bbdc1264bcb303c3438f01fbcdd
KshitijV97 <writetokshitij@gmail.com>
2022-05-05 08:00:44 +0530

    Made an  array of available images

---
+++ src/helpers/CarouselData.js
@@ -0,0 +1,21 @@
+import City from '../images/city.jpg';
+import Salvador from '../images/city.jpg';
+import Ubc from '../images/city.jpg';
+
+export const images = [
+	{
+		title: 'San Diego',
+		subtitle: 'This is San Diego',
+		img: City,
+	},
+	{
+		title: 'Salvador, Brazil',
+		subtitle: 'The best city in the world',
+		img: Salvador,
+	},
+	{
+		title: 'San Diego',
+		subtitle: 'This is San Diego',
+		img: Ubc,
+	},
+];
