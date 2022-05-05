c08c4b5d5a018ac8b6769485d18c42fbe6875489
KshitijV97 <writetokshitij@gmail.com>
2022-05-05 10:06:01 +0530

    Changing image on click

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Carousel.css';
 
 import { images } from '../helpers/CarouselData';
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 
 function Carousel() {
 	const [currentImage, setCurrentImage] = useState(0);
@@ -11,9 +12,24 @@ function Carousel() {
 				className='carouselInner'
 				style={{ backgroundImage: `url(${images[currentImage].img})` }}
 			>
-				<div className='left'></div>
+				<div
+					className='left'
+					onClick={() => {
+						currentImage > 0 && setCurrentImage(currentImage - 1);
+					}}
+				>
+					<ArrowBackIos />
+				</div>
 				<div className='center'></div>
-				<div className='right'></div>
+				<div
+					className='right'
+					onClick={() => {
+						currentImage < images.length - 1 &&
+							setCurrentImage(currentImage + 1);
+					}}
+				>
+					<ArrowForwardIos />
+				</div>
 			</div>
 		</div>
 	);
